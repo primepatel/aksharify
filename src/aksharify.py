74c727f02e1c6961d4c28d0fe0725b5c722e46b7
Prime Patel <primespatel@gmail.com>
2023-05-28 11:17:42 +0530

    v2.3.2 svg output added

--- src/aksharify.py
+++ src/aksharify.py
@@ -6,8 +6,9 @@ class TextArt:
         self.image = Image.open(image)
         self.w, self.h = self.image.size
         self.ascii_chars = list('01')
-        self.ascii_text = ''
+        self.ascii_txt = ''
         self.ascii_html = ''
+        self.ascii_svg = ''
 
     def set_dim(self, width=0, height=0):
         if width != 0 and height == 0:
@@ -28,6 +29,9 @@ class TextArt:
 
     def span(self, integer, integer_colour):
         return f"<span style='color: rgb{integer_colour};'><b>{integer}</b></span>"
+    
+    def tspan(self, char, char_color, x):
+        return f'<tspan x="{x}" fill="{char_color}">{char}</tspan>'
 
     def asciify(self):
         self.ascii_text = ""
@@ -76,13 +80,41 @@ class TextArt:
             text += self.ascii_text[line_no*self.w:line_no*self.w + self.w] + "\n"
         print(text)
 
-    def text_output(self, fname):
+    def txt_output(self, fname):
         text = ""
         for line_no in range(self.h):
             text += self.ascii_text[line_no*self.w:line_no*self.w + self.w] + "\n"
         with open(fname + ".txt", "w") as file:
             file.write(text)
 
-    def color_output(self, fname):
+    def html_output(self, fname):
         with open(fname + ".html", "w") as file:
             file.write(self.ascii_html)
+    
+    def rgb2hex(self, rgba):
+        return '#{:02x}{:02x}{:02x}'.format(rgba[0], rgba[1], rgba[2])
+    
+    def svgify(self):
+        color = self.image.getdata()
+        if self.ascii_text[:2] == "0b":
+            self.ascii_text = self.ascii_text[2:]
+        file = f'<?xml version="1.0" standalone="no"?><svg width="{int(self.w*11.087)+41}" height="{int(self.h*20)+41}" version="1.1" xmlns="http://www.w3.org/2000/svg" style="font-family: monospace; background-color:#ffffff; font-size:20;"><desc>Aksharify Art</desc>'
+        #file += f'<rect x="20" y="20" width="{int(self.w*11.087)+21}" height="{int(self.h*20)+21}" fill="none" stroke="blue" stroke-width="2" />'
+        file += f'<a href="https://primepatel.github.io/aksharify-docs/">'
+        y = 30
+        x = 20
+        for line_no in range(self.h):
+            file += f'<text x="{x}" y="{y}">'
+            for pixel in range(line_no*self.w, line_no*self.w + self.w):
+                file += self.tspan(self.ascii_text[pixel], self.rgb2hex(color[pixel]), x)
+                x += 11.1
+            file += '</text>'
+            x = 20
+            y += 20
+        #file += "</a>"
+        file += "</svg>"
+        self.ascii_svg = file
+        
+    def svg_output(self, fname):
+        with open(fname + ".svg", "w") as file:
+            file.write(self.ascii_svg)
